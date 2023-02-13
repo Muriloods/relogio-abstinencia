43a836fd4c09b0efdd04d2be3b5ed87867d0198c
Murilo Oliveira <muriloodss@gmail.com>
2023-02-12 23:21:57 -0300

    criando edicao

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,17 @@
 <script setup>
 import { useRouter } from 'vue-router'
 import LocalBase from 'localbase'
+import { onMounted } from 'vue'
 
-const router = useRouter()
-const db = new LocalBase('relogio-sobriedade')
-const retorno = await db.collection('dadosUsuario').get({ keys: true })
+onMounted(async () => {
+  const router = useRouter()
+  const db = new LocalBase()
+  const retorno = await db.collection('dadosUsuario').get({ keys: true })
+  if (retorno.length > 0) {
+    router.push({ path: '/dateCounter' })
+  }
+})
 
-if (retorno.length > 0) {
-  router.push({ path: '/dateCounter' })
-}
 </script>
 <template>
     <q-page class="flex flex-center">
